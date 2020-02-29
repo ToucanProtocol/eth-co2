a96eb6c2fc36407619402368b259c366128e251c
Adam Spiers <blockchain@adamspiers.org>
2020-02-29 18:32:59 +0000

    Initial prototype

---
+++ README.md
@@ -0,0 +1,69 @@
+# web3-co2
+
+`web3-co2.js` is a JavaScript library to enable offsetting of CO2 emissions via
+the Ethereum blockchain.
+
+## Building
+
+    npm run build
+
+## Example usage
+
+### Calculating the cost of offsetting CO2 emissions
+
+For example, to calculate the cost of offsetting a flight from London to New York:
+
+```javascript
+import "babel-polyfill";
+import { ethers } from "ethers";
+
+import { getCO2kenPrice } from "./ts/web3-co2";
+
+const dapp = async () => {
+  let provider = new ethers.providers.Web3Provider(web3.currentProvider);
+  window.price = await getCO2kenPrice(provider);
+  let flightEmissions = 500;  // approx kg CO2 emissions from LON -> NYC flight
+  let co2kenPrice = await getCO2kenPrice(web3);  // Cost in DAI to offset 1 ton of CO2
+  let offsetCost = flightEmissions / 1000 * co2kenPrice;
+};
+
+dapp().catch(e => { console.error(e) });
+
+```
+
+### Pay DAI to offset carbon emissions
+
+```javascript
+import { offsetEmissions } from "@CO2ken/web3-co2";
+
+offsetEmissions(web3, dai);
+
+```
+
+### Obtain gas footprint
+
+
+```javascript
+import { getCO2kenPrice } from "@CO2ken/web3-co2";
+
+let gasFootprint = await getFootprint(web3);  // CO2 emissions per gas
+```
+
+
+### Estimate emissions from a transaction
+
+```javascript
+let emissions = gasUsed * gasFootprint;
+let offsetCost = emissions * co2kenPrice;
+```
+
+### Wrap it all up for convenience
+
+```javascript
+import { estimateEmissions } from "@CO2ken/web3-co2";
+
+let emissions = await estimateEmissions(web3, contractName, functionName);
+
+
+```
+
---
+++ src/ts/web3-co2.ts
@@ -0,0 +1,258 @@
+import { Contract } from "ethers";
+import { Provider } from "ethers/providers";
+
+const ethers = require('ethers');
+
+function getContract (provider) {
+  // CO2kenData.sol on Rinkeby
+  let contractAddress = "0x127AE08f45d687dA7887ceA369F2f4D95cb9baf2";
+  let abi = [
+    {
+      "inputs": [
+        {
+          "internalType": "uint256",
+          "name": "_co2kenPrice",
+          "type": "uint256"
+        }
+      ],
+      "stateMutability": "nonpayable",
+      "type": "constructor"
+    },
+    {
+      "anonymous": false,
+      "inputs": [
+        {
+          "indexed": false,
+          "internalType": "uint256",
+          "name": "newPrice",
+          "type": "uint256"
+        }
+      ],
+      "name": "CO2kenPriceChanged",
+      "type": "event"
+    },
+    {
+      "anonymous": false,
+      "inputs": [
+        {
+          "indexed": false,
+          "internalType": "uint256",
+          "name": "newGasCarbonFootprint",
+          "type": "uint256"
+        }
+      ],
+      "name": "GasCarbonFootprintChanged",
+      "type": "event"
+    },
+    {
+      "anonymous": false,
+      "inputs": [
+        {
+          "indexed": false,
+          "internalType": "uint256",
+          "name": "newGasEnergyFootprint",
+          "type": "uint256"
+        }
+      ],
+      "name": "GasEnergyFootprintChanged",
+      "type": "event"
+    },
+    {
+      "anonymous": false,
+      "inputs": [
+        {
+          "indexed": false,
+          "internalType": "uint256",
+          "name": "newGridEmissionFactor",
+          "type": "uint256"
+        }
+      ],
+      "name": "GridEmissionFactorChanged",
+      "type": "event"
+    },
+    {
+      "anonymous": false,
+      "inputs": [
+        {
+          "indexed": true,
+          "internalType": "address",
+          "name": "previousOwner",
+          "type": "address"
+        },
+        {
+          "indexed": true,
+          "internalType": "address",
+          "name": "newOwner",
+          "type": "address"
+        }
+      ],
+      "name": "OwnershipTransferred",
+      "type": "event"
+    },
+    {
+      "inputs": [],
+      "name": "co2kenPrice",
+      "outputs": [
+        {
+          "internalType": "uint256",
+          "name": "",
+          "type": "uint256"
+        }
+      ],
+      "stateMutability": "view",
+      "type": "function"
+    },
+    {
+      "inputs": [],
+      "name": "gasCarbonFootprint",
+      "outputs": [
+        {
+          "internalType": "uint256",
+          "name": "",
+          "type": "uint256"
+        }
+      ],
+      "stateMutability": "view",
+      "type": "function"
+    },
+    {
+      "inputs": [],
+      "name": "gasEnergyFootprint",
+      "outputs": [
+        {
+          "internalType": "uint256",
+          "name": "",
+          "type": "uint256"
+        }
+      ],
+      "stateMutability": "view",
+      "type": "function"
+    },
+    {
+      "inputs": [],
+      "name": "gridEmissionFactor",
+      "outputs": [
+        {
+          "internalType": "uint256",
+          "name": "",
+          "type": "uint256"
+        }
+      ],
+      "stateMutability": "view",
+      "type": "function"
+    },
+    {
+      "inputs": [],
+      "name": "isOwner",
+      "outputs": [
+        {
+          "internalType": "bool",
+          "name": "",
+          "type": "bool"
+        }
+      ],
+      "stateMutability": "view",
+      "type": "function"
+    },
+    {
+      "inputs": [],
+      "name": "owner",
+      "outputs": [
+        {
+          "internalType": "address",
+          "name": "",
+          "type": "address"
+        }
+      ],
+      "stateMutability": "view",
+      "type": "function"
+    },
+    {
+      "inputs": [],
+      "name": "renounceOwnership",
+      "outputs": [],
+      "stateMutability": "nonpayable",
+      "type": "function"
+    },
+    {
+      "inputs": [
+        {
+          "internalType": "uint256",
+          "name": "_co2kenPrice",
+          "type": "uint256"
+        }
+      ],
+      "name": "setCO2kenPrice",
+      "outputs": [],
+      "stateMutability": "nonpayable",
+      "type": "function"
+    },
+    {
+      "inputs": [
+        {
+          "internalType": "uint256",
+          "name": "_gasCarbonFootprint",
+          "type": "uint256"
+        }
+      ],
+      "name": "setGasCarbonFootprint",
+      "outputs": [],
+      "stateMutability": "nonpayable",
+      "type": "function"
+    },
+    {
+      "inputs": [
+        {
+          "internalType": "uint256",
+          "name": "_gasEnergyFootprint",
+          "type": "uint256"
+        }
+      ],
+      "name": "setGasEnergyFootprint",
+      "outputs": [],
+      "stateMutability": "nonpayable",
+      "type": "function"
+    },
+    {
+      "inputs": [
+        {
+          "internalType": "uint256",
+          "name": "_gridEmissionFactor",
+          "type": "uint256"
+        }
+      ],
+      "name": "setGridEmissionFactor",
+      "outputs": [],
+      "stateMutability": "nonpayable",
+      "type": "function"
+    },
+    {
+      "inputs": [
+        {
+          "internalType": "address",
+          "name": "newOwner",
+          "type": "address"
+        }
+      ],
+      "name": "transferOwnership",
+      "outputs": [],
+      "stateMutability": "nonpayable",
+      "type": "function"
+    }
+  ];
+
+  // We connect to the Contract using a Provider, so we will only
+  // have read-only access to the Contract
+  let contract = new ethers.Contract(contractAddress, abi, provider);
+
+  return contract;
+}
+
+/**
+ * Get current CO2ken price in DAI
+ * @param provider Provider object
+ */
+export async function getCO2kenPrice (provider: Provider) {
+  let contract = getContract(provider);
+  return await contract.co2kenPrice();
+}
