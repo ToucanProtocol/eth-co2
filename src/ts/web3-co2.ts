3d4b36be9bd01a491d865d8d73f7eb9f307eb515
benesjan <janbenes1234@gmail.com>
2020-03-01 03:19:57 +0100

    offsetCarbon function call

--- src/ts/web3-co2.ts
+++ src/ts/web3-co2.ts
@@ -3,7 +3,280 @@ import { Provider } from "ethers/providers";
 
 const ethers = require('ethers');
 
-function getContract (provider) {
+function getTokenContract (provider) {
+  // CO2kenData.sol on Rinkeby
+  let contractAddress = "0x6a1B0C693DD4AA99bA8E93247AA221Fb30525Cfe";
+  let abi = [
+    {
+      "inputs": [
+        {
+          "internalType": "address",
+          "name": "storageTarget",
+          "type": "address"
+        },
+        {
+          "internalType": "address",
+          "name": "daiTarget",
+          "type": "address"
+        },
+        {
+          "internalType": "string",
+          "name": "name",
+          "type": "string"
+        },
+        {
+          "internalType": "string",
+          "name": "symbol",
+          "type": "string"
+        },
+        {
+          "internalType": "uint8",
+          "name": "decimals",
+          "type": "uint8"
+        }
+      ],
+      "stateMutability": "nonpayable",
+      "type": "constructor"
+    },
+    {
+      "anonymous": false,
+      "inputs": [
+        {
+          "indexed": true,
+          "internalType": "address",
+          "name": "from",
+          "type": "address"
+        },
+        {
+          "indexed": false,
+          "internalType": "uint256",
+          "name": "value",
+          "type": "uint256"
+        }
+      ],
+      "name": "CarbonOffsetted",
+      "type": "event"
+    },
+    {
+      "anonymous": false,
+      "inputs": [
+        {
+          "indexed": false,
+          "internalType": "uint256",
+          "name": "payment",
+          "type": "uint256"
+        },
+        {
+          "indexed": false,
+          "internalType": "uint256",
+          "name": "price",
+          "type": "uint256"
+        }
+      ],
+      "name": "Debug",
+      "type": "event"
+    },
+    {
+      "anonymous": false,
+      "inputs": [
+        {
+          "indexed": false,
+          "internalType": "string",
+          "name": "ipfsHash",
+          "type": "string"
+        },
+        {
+          "indexed": false,
+          "internalType": "uint256",
+          "name": "dollarValue",
+          "type": "uint256"
+        },
+        {
+          "indexed": false,
+          "internalType": "uint256",
+          "name": "tokensMinted",
+          "type": "uint256"
+        }
+      ],
+      "name": "Minted",
+      "type": "event"
+    },
+    {
+      "anonymous": false,
+      "inputs": [
+        {
+          "indexed": true,
+          "internalType": "address",
+          "name": "previousOwner",
+          "type": "address"
+        },
+        {
+          "indexed": true,
+          "internalType": "address",
+          "name": "newOwner",
+          "type": "address"
+        }
+      ],
+      "name": "OwnershipTransferred",
+      "type": "event"
+    },
+    {
+      "anonymous": false,
+      "inputs": [
+        {
+          "indexed": false,
+          "internalType": "uint256",
+          "name": "value",
+          "type": "uint256"
+        }
+      ],
+      "name": "Withdrawal",
+      "type": "event"
+    },
+    {
+      "inputs": [],
+      "name": "balance",
+      "outputs": [
+        {
+          "internalType": "uint256",
+          "name": "",
+          "type": "uint256"
+        }
+      ],
+      "stateMutability": "view",
+      "type": "function"
+    },
+    {
+      "inputs": [],
+      "name": "decimals",
+      "outputs": [
+        {
+          "internalType": "uint8",
+          "name": "",
+          "type": "uint8"
+        }
+      ],
+      "stateMutability": "view",
+      "type": "function"
+    },
+    {
+      "inputs": [],
+      "name": "isOwner",
+      "outputs": [
+        {
+          "internalType": "bool",
+          "name": "",
+          "type": "bool"
+        }
+      ],
+      "stateMutability": "view",
+      "type": "function"
+    },
+    {
+      "inputs": [
+        {
+          "internalType": "string",
+          "name": "ipfsHash",
+          "type": "string"
+        },
+        {
+          "internalType": "uint256",
+          "name": "amountTokens",
+          "type": "uint256"
+        }
+      ],
+      "name": "mint",
+      "outputs": [],
+      "stateMutability": "nonpayable",
+      "type": "function"
+    },
+    {
+      "inputs": [],
+      "name": "name",
+      "outputs": [
+        {
+          "internalType": "string",
+          "name": "",
+          "type": "string"
+        }
+      ],
+      "stateMutability": "view",
+      "type": "function"
+    },
+    {
+      "inputs": [
+        {
+          "internalType": "uint256",
+          "name": "payment",
+          "type": "uint256"
+        }
+      ],
+      "name": "offsetCarbon",
+      "outputs": [],
+      "stateMutability": "nonpayable",
+      "type": "function"
+    },
+    {
+      "inputs": [],
+      "name": "owner",
+      "outputs": [
+        {
+          "internalType": "address",
+          "name": "",
+          "type": "address"
+        }
+      ],
+      "stateMutability": "view",
+      "type": "function"
+    },
+    {
+      "inputs": [],
+      "name": "renounceOwnership",
+      "outputs": [],
+      "stateMutability": "nonpayable",
+      "type": "function"
+    },
+    {
+      "inputs": [],
+      "name": "symbol",
+      "outputs": [
+        {
+          "internalType": "string",
+          "name": "",
+          "type": "string"
+        }
+      ],
+      "stateMutability": "view",
+      "type": "function"
+    },
+    {
+      "inputs": [
+        {
+          "internalType": "address",
+          "name": "newOwner",
+          "type": "address"
+        }
+      ],
+      "name": "transferOwnership",
+      "outputs": [],
+      "stateMutability": "nonpayable",
+      "type": "function"
+    },
+    {
+      "inputs": [],
+      "name": "withdraw",
+      "outputs": [],
+      "stateMutability": "nonpayable",
+      "type": "function"
+    }
+  ];
+
+  // We connect to the Contract using a Provider, so we will only
+  // have read-only access to the Contract
+  return new ethers.Contract(contractAddress, abi, provider);
+}
+
+function getStorageContract (provider) {
   // CO2kenData.sol on Rinkeby
   let contractAddress = "0x127AE08f45d687dA7887ceA369F2f4D95cb9baf2";
   let abi = [
@@ -251,7 +524,7 @@ function getContract (provider) {
  * @param provider Provider object
  */
 export async function getCo2kenPrice (provider: Provider) {
-  let contract = getContract(provider);
+  let contract = getStorageContract(provider);
   return await contract.co2kenPrice();
 }
 
@@ -260,7 +533,7 @@ export async function getCo2kenPrice (provider: Provider) {
  * @param provider Provider object
  */
 export async function getGasEnergyFootprint (provider: Provider) {
-  let contract = getContract(provider);
+  let contract = getStorageContract(provider);
   return await contract.gasEnergyFootprint();
 }
 
@@ -269,7 +542,7 @@ export async function getGasEnergyFootprint (provider: Provider) {
  * @param provider Provider object
  */
 export async function getGridEmissionFactor (provider: Provider) {
-  let contract = getContract(provider);
+  let contract = getStorageContract(provider);
   return await contract.gridEmissionFactor();
 }
 
@@ -278,6 +551,16 @@ export async function getGridEmissionFactor (provider: Provider) {
  * @param provider Provider object
  */
 export async function getGasCarbonFootprint (provider: Provider) {
-  let contract = getContract(provider);
+  let contract = getStorageContract(provider);
   return await contract.gasCarbonFootprint();
+}
+
+/**
+ * Get gasCarbonFootprint
+ * @param provider Provider object
+ * @param payment
+ */
+export async function offsetCarbon (provider: Provider, payment: number) {
+  let contract = getTokenContract(provider);
+  return await contract.offsetCarbon(payment);
 }
